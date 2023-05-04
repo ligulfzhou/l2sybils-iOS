f95eb275e080281b042c7d3a9de1a562d24874f4
ligulfzhou <ligulfzhou53@gmail.com>
2023-05-04 10:26:06 +0800

    Initial Commit

---
+++ L2Sybils/L2SybilsApp.swift
@@ -0,0 +1,17 @@
+//
+//  L2SybilsApp.swift
+//  L2Sybils
+//
+//  Created by Ligang Zhou on 2023/5/4.
+//
+
+import SwiftUI
+
+@main
+struct L2SybilsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
